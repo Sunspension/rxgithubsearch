46e5a05afc893aefa41cb0c7fae2c7f9571f87e0
Vladimir Kokhanevich <vladimir.kokhanevich@gmail.com>
2020-06-08 09:02:10 -0700

    Initial Commit

---
+++ RxSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSearchApp
+//
+//  Created by Vladimir Kokhanevich on 08.06.2020.
+//  Copyright © 2020 Vladimir Kokhanevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
